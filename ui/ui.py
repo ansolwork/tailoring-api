ace76ba8eaa47a63879347582ddc84c1f0882211
avinashmohanraju <avinash@theaiframework.com>
2024-10-01 16:35:24 +0200

    Fixed filename,0bytes and duplicates

--- ui/ui.py
+++ ui/ui.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, redirect, send_from_directory
+from flask import Flask, request, jsonify, render_template, redirect, send_from_directory, flash
 import os
 import yaml
 from utils.aws_utils import AwsUtils
@@ -13,8 +13,7 @@ import pandas as pd
 
 # Run from project root (no relative path needed)
 config_filepath = "tailoring_api_config.yml"
-#test_profile = "183295423477_PowerUserAccess"
-
+# test_profile = "183295423477_PowerUserAccess"
 
 
 app = Flask(__name__, template_folder="templates")
@@ -38,9 +37,10 @@ AWS_MTM_DIR_PATH_LABELED = yaml_config['AWS_MTM_DIR_PATH_LABELED']
 AWS_OUTPUT_DIR_PATH = yaml_config['AWS_OUTPUT_DIR_PATH']
 AWS_S3_SIGNATURE_VERSION = yaml_config['AWS_S3_SIGNATURE_VERSION']
 AWS_PLOT_DIR_BASE = yaml_config['AWS_PLOT_DIR_BASE']
-AWS_PROFILE= yaml_config['AWS_PROFILE']
+AWS_PROFILE = yaml_config['AWS_PROFILE']
 
-aws_utils = AwsUtils(ALLOWED_EXTENSIONS, ALLOWED_MIME_TYPES, AWS_S3_BUCKET_NAME, AWS_S3_SIGNATURE_VERSION,AWS_PROFILE)
+aws_utils = AwsUtils(ALLOWED_EXTENSIONS, ALLOWED_MIME_TYPES, AWS_S3_BUCKET_NAME, AWS_S3_SIGNATURE_VERSION, AWS_PROFILE)
+
 
 # Function to clear static/plots folder
 def clear_static_plots_folder(folder_path="ui/static/plots/"):
@@ -54,12 +54,14 @@ def clear_static_plots_folder(folder_path="ui/static/plots/"):
         except Exception as e:
             print(f"Failed to delete {file_path}. Reason: {e}")
 
+
 @app.route("/home")
 def home():
     # return "Hello World!"
     contents_output = aws_utils.list_all_s3_files(AWS_OUTPUT_DIR_PATH)
     contents_mtm = aws_utils.list_all_s3_files(AWS_MTM_DIR_PATH)
     return render_template("Index.html", contents_output=contents_output, contents_mtm=contents_mtm)
+
 
 @app.route("/upload_file", methods=["POST", "GET"])
 def upload_file():
@@ -77,12 +79,19 @@ def upload_file():
 
         if typeform.lower() == 'dxf_file':
             print(f"Processing DXF file: {file.filename}")
+            with tempfile.TemporaryDirectory() as tmpdirname:
+                # Create a temp file with the same name as the uploaded file
+                temp_file_path = os.path.join(tmpdirname, file.filename)
 
-            with tempfile.NamedTemporaryFile(delete=False, suffix=".dxf") as temp_file:
-                file.save(temp_file.name)  # Save the uploaded file to the temp file
+                # Save the uploaded file to the temp file
+                file.save(temp_file_path)
+
+                # with tempfile.NamedTemporaryFile(delete=False, suffix=".dxf") as temp_file:
+                # file.save(temp_file.name)  # Save the uploaded file to the temp file
 
                 # Load DXF using the file path
-                dxf_loader.load_dxf(temp_file.name)
+                # dxf_loader.load_dxf(temp_file.name)
+                dxf_loader.load_dxf(temp_file_path)
 
                 # Convert DXF entities to a Pandas DataFrame
                 df = dxf_loader.entities_to_dataframe()
@@ -97,10 +106,14 @@ def upload_file():
                 # Draw lines
                 lines = df[df['Type'] == 'LINE']
                 for _, row in lines.iterrows():
-                    if pd.notna(row['Line_Start_X']) and pd.notna(row['Line_End_X']) and pd.notna(row['Line_Start_Y']) and pd.notna(row['Line_End_Y']):
-                        plt.plot([row['Line_Start_X'], row['Line_End_X']], [row['Line_Start_Y'], row['Line_End_Y']], marker='o', linewidth=0.5, markersize=5)
-                        plt.text(row['Line_Start_X'], row['Line_Start_Y'], f"({row['Line_Start_X']}, {row['Line_Start_Y']})", fontsize=8, ha='right', va='bottom')
-                        plt.text(row['Line_End_X'], row['Line_End_Y'], f"({row['Line_End_X']}, {row['Line_End_Y']})", fontsize=8, ha='right', va='bottom')
+                    if pd.notna(row['Line_Start_X']) and pd.notna(row['Line_End_X']) and pd.notna(
+                            row['Line_Start_Y']) and pd.notna(row['Line_End_Y']):
+                        plt.plot([row['Line_Start_X'], row['Line_End_X']], [row['Line_Start_Y'], row['Line_End_Y']],
+                                 marker='o', linewidth=0.5, markersize=5)
+                        plt.text(row['Line_Start_X'], row['Line_Start_Y'],
+                                 f"({row['Line_Start_X']}, {row['Line_Start_Y']})", fontsize=8, ha='right', va='bottom')
+                        plt.text(row['Line_End_X'], row['Line_End_Y'], f"({row['Line_End_X']}, {row['Line_End_Y']})",
+                                 fontsize=8, ha='right', va='bottom')
 
                 # Draw polylines
                 polylines = df[df['Type'].isin(['POLYLINE', 'LWPOLYLINE'])]
@@ -112,7 +125,8 @@ def upload_file():
                     plt.plot(xs, ys, marker='o', linewidth=0.5, markersize=5)
 
                 # Annotate unique points
-                for x, y, point_label in zip(unique_points['PL_POINT_X'], unique_points['PL_POINT_Y'], unique_points['Point Label']):
+                for x, y, point_label in zip(unique_points['PL_POINT_X'], unique_points['PL_POINT_Y'],
+                                             unique_points['Point Label']):
                     plt.text(x, y, f'{point_label}', fontsize=8, ha='right', va='bottom')
 
                 # Configure and save the plot
@@ -131,7 +145,8 @@ def upload_file():
                 plt.close(fig)
 
                 # Upload plot to S3
-                s3_plot_path = os.path.join(AWS_PLOT_DIR_BASE + "/for_labeling", f"{os.path.splitext(file.filename)[0]}_plot.png")
+                s3_plot_path = os.path.join(AWS_PLOT_DIR_BASE + "/for_labeling",
+                                            f"{os.path.splitext(file.filename)[0]}_plot.png")
                 aws_utils.upload_file_by_path_to_s3(output_plot_path, s3_plot_path)  # Upload plot to S3
 
                 # Generate presigned URL for the plot
@@ -144,27 +159,42 @@ def upload_file():
                 base_filename = os.path.splitext(os.path.basename(file.filename))[0]
                 aws_mtm_dir_path = os.path.join(AWS_MTM_DIR_PATH, f"{base_filename}_combined_entities.xlsx")
 
-            # Delete the temp file after processing (optional)
-            os.remove(temp_file.name)
+                with open(temp_file_path, 'rb') as tmp:
+                    #check for duplicate file using hash value of the file
+                    file_content = tmp.read()
+                    file_hash = aws_utils.compute_file_hashValue(file_content)
+                    tmp.seek(0)
+                    if not aws_utils.check_hash_exists(file_hash,AWS_DXF_DIR_PATH):
+                        aws_utils.upload_file_to_s3(tmp, AWS_DXF_DIR_PATH + file.filename)
+                        aws_utils.update_hash_file(AWS_DXF_DIR_PATH)
+                        print(f'Successfully uploaded {file.filename} to S3!')
+                    else:
+                        print(f'DUPLICATE file : {file.filename} ')
+                        return "DUPLICATE file detected. Upload a different file"
 
-            # This needs to happen after temp file is closed
-            aws_utils.upload_file_to_s3(file, AWS_DXF_DIR_PATH)
             aws_utils.upload_dataframe_to_s3(sorted_df, aws_mtm_dir_path, file_format="excel")
-        
+
             # Generate a presigned URL for the CSV file
             presigned_csv_url = aws_utils.generate_presigned_url(aws_mtm_dir_path)
 
-            #return render_template("download.html", download_url=presigned_url)
-            return render_template("display_and_download_dxf.html", plot_url=presigned_plot_url, csv_url=presigned_csv_url)
+            # return render_template("download.html", download_url=presigned_url)
+            return render_template("display_and_download_dxf.html", plot_url=presigned_plot_url,
+                                   csv_url=presigned_csv_url)
 
         if typeform.lower() == 'mtm_points_file':
             print(f"Processing MTM points file: {file.filename}")
-            with tempfile.NamedTemporaryFile(delete=False, suffix=".dxf") as temp_file:
-                file.save(temp_file.name)
+            # with tempfile.NamedTemporaryFile(delete=False, suffix=".dxf") as temp_file:
+            # file.save(temp_file.name)
+            with tempfile.TemporaryDirectory() as tmpdirname:
+                # Create a temp file with the same name as the uploaded file
+                temp_file_path = os.path.join(tmpdirname, file.filename)
+
+                # Save the uploaded file to the temp file
+                file.save(temp_file_path)
                 mtm_processor = MTMProcessor()
-                mtm_processor.load_coordinates_tables(temp_file.name)
+                mtm_processor.load_coordinates_tables(temp_file_path)
                 mtm_processor.remove_nan_mtm_points()
-                #mtm_processor.display_filtered_coordinates_tables()
+                # mtm_processor.display_filtered_coordinates_tables()
 
                 # Remove the extension
                 table_name = os.path.splitext(file.filename)[0]
@@ -172,15 +202,29 @@ def upload_file():
                 print(f"Piece Name: {piece_name}")
 
                 plot_filename, plot_file_path = mtm_processor.plot_points(rename=piece_name)
-                
+
                 aws_mtm_plot_dir_path = os.path.join(AWS_PLOT_DIR_BASE + "/labeled_mtm/", f"{plot_filename}_base.png")
                 aws_utils.upload_file_by_path_to_s3(plot_file_path, aws_mtm_plot_dir_path)
                 print(f"Plot Filename {plot_filename}")
 
-            # Delete the temp file after processing (optional)
-            os.remove(temp_file.name)     
+                with open(temp_file_path, 'rb') as tmp:
+                    # check for duplicate file using hash value of the file
+                    file_content = tmp.read()
+                    file_hash = aws_utils.compute_file_hashValue(file_content)
+                    tmp.seek(0)
+                    if not aws_utils.check_hash_exists(file_hash, AWS_MTM_DIR_PATH_LABELED):
+                        aws_utils.upload_file_to_s3(tmp, AWS_MTM_DIR_PATH_LABELED + file.filename)
+                        aws_utils.update_hash_file(AWS_MTM_DIR_PATH_LABELED)
+                        print(f'Successfully uploaded {file.filename} to S3!')
+                    else:
+                        print(f'DUPLICATE file : {file.filename} ')
+                        return "DUPLICATE file detected. Upload a different file"
 
-            aws_utils.upload_file_to_s3(file, AWS_MTM_DIR_PATH_LABELED)
+
+            # Delete the temp file after processing (optional)
+            # os.remove(temp_file.name)
+
+            # aws_utils.upload_file_to_s3(file, AWS_MTM_DIR_PATH_LABELED)
 
             # Generate a presigned URL for the plot file in S3
             plot_presigned_url = aws_utils.generate_presigned_url(aws_mtm_plot_dir_path)
@@ -189,8 +233,8 @@ def upload_file():
             clear_static_plots_folder()
 
             # Render the plot on the page by passing the presigned URL to the template
-            return render_template("display_mtm_plot.html", plot_url=plot_presigned_url)   
-                 
+            return render_template("display_mtm_plot.html", plot_url=plot_presigned_url)
+
         return redirect("/home")
     else:
         return "File not uploaded successfully"
