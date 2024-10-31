f92d5521bee123639843244ec3b5b4a78b60113f
avinashmohanraju <avinash@theaiframework.com>
2024-10-31 16:05:34 +0100

    moving DB details to env variable for AWS deployment

--- app/api.py
+++ app/api.py
@@ -24,9 +24,9 @@ DB_TYPE = yaml_config['DB_TYPE']
 DB_API = yaml_config['DB_API']
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = yaml_config['DB_HOST']
-DB_PORT = yaml_config['DB_PORT']
-DB_NAME = yaml_config['DB_NAME']
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
 
 connection_string = f"{DB_TYPE}+{DB_API}://{DB_USER}:{urllib.parse.quote(DB_PASSWORD)}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
